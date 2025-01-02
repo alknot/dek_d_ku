2fa77b3070a8f4503613d0cbec85114a828a6cf2
alknot <alknotsad@gmail.com>
2025-01-02 15:48:12 +0700

    test add auth

---
+++ app/auth/login/logout/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+    return <div>This is Logout Page</div>;
+  };
+  export default Page;
+  
---
+++ app/auth/login/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+    return <div>This is Login Page</div>;
+  };
+  export default Page;
+  

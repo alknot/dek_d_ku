1654abd1e70e3661dd89c9eb129f37fb25176f29
Punyaphat Surakiatkamjorn <punyaphat.sura@gmail.com>
2025-01-03 20:13:58 +0700

    Update model

---
+++ app/types/user.ts
@@ -0,0 +1,16 @@
+export enum TypePerson {
+  TEACHER = '1', // Teacher
+  STAFF = '2', // Staff
+  STUDENT = '3', // Student
+  ALUMNI = '4', // Alumni
+  GUEST = '5', // Guest
+  EMAILFAC = '6', // บัญชีอีเมลหน่วยงาน
+  LAND_SCIENCE_STUDENT = '7', // นิสิต หลักสูตรศาสตร์แห่งแผ่นดิน
+  NON_DEGREE = '8', // นิสิต ข้ามสถาบัน
+  TEACHER_SCHOOL_KASETSART_SPECIAL = '101', // อาจารย์โรงเรียนสาธิตเกษตรภาคพิเศษ
+  STAFF_SCHOOL_KASETSART_SPECIAL = '102', // บุคลากรโรงเรียนสาธิตเกษตรภาคพิเศษ
+  STUDENT_SCHOOL_KASETSART_SPECIAL = '103', // นักเรียนโรงเรียนสาธิตเกษตรภาคพิเศษ
+  TEACHER_SCHOOL_KASETSART_SPECIAL_KAMPHAENGSAEN = '111', // อาจารย์โรงเรียนสาธิตเกษตรภาคพิเศษ กำแพงแสน
+  STAFF_SCHOOL_KASETSART_SPECIAL_KAMPHAENGSAEN = '112', // บุคลากรโรงเรียนสาธิตเกษตรภาคพิเศษ กำแพงแสน
+  STUDENT_SCHOOL_KASETSART_SPECIAL_KAMPHAENGSAEN = '113', // นักเรียนโรงเรียนสาธิตเกษตรภาคพิเศษ กำแพงแสน
+}

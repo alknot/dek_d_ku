02a08f74bbbff30080da015e25bd9051a32865aa
Punyaphat Surakiatkamjorn <punyaphat.sura@gmail.com>
2025-01-03 20:13:47 +0700

    Move middleware to config

--- app/libs/api/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-// Middleware to handle CORS headers
-export const setCorsHeaders = (response: NextResponse) => {
-  response.headers.set('Access-Control-Allow-Origin', '*'); // Allow all origins, or replace '*' with specific domains
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUSH, DELETE, PATCH'); // Allowed HTTP methods
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type'); // Allowed headers
-  return response;
-};
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
